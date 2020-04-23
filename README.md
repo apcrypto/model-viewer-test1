0db24b6d2c50df3ce409bf21e4381499792deab4
Andy Purbrick <andypurbrick@gmail.com>
2020-04-23 17:09:55 +0100

    added orbit values

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# model-viewer-test1
+
---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let toolbar = document.querySelector(".toolbar");
+let wrapper = document.querySelector(".model-viewer-wrapper");
+let modelViewer = document.querySelector("#orbit-test");
+toolbar.addEventListener("click", eventHandler);
+
+function eventHandler(e) {
+  let position = e.target.attributes[0].value;
+  let att = document.createAttribute("camera-orbit");
+  if (position == "position1") {
+    att.value = "90deg auto 50%";
+    modelViewer.setAttributeNode(att);
+  } else if (position == "position2") {
+    att.value = "auto -180deg auto";
+    modelViewer.setAttributeNode(att);
+  } else if (position == "position3") {
+    att.value = "auto 180deg auto";
+    modelViewer.setAttributeNode(att);
+  } else if (position == "position4") {
+    att.value = "auto 90deg auto";
+    modelViewer.setAttributeNode(att);
+  }
+}
