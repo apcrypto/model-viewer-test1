aa7a5d1383a19e282e868e4495818c3e10deb349
apcrypto <andypurbrick@gmail.com>
2025-02-01 12:43:46 +0000

    refactored code

--- scripts/index.js
+++ scripts/index.js
@@ -1,92 +1,21 @@
-const position1 = document.querySelector(".position1");
-const position2 = document.querySelector(".position2");
-const position3 = document.querySelector(".position3");
-const position4 = document.querySelector(".position4");
+const positions = {
+  position1: { orbit: "90deg auto 80%", element: ".position1" },
+  position2: { orbit: "auto -180deg 90%", element: ".position2" },
+  position3: { orbit: "-90deg 170deg 85%", element: ".position3" },
+  position4: { orbit: "auto auto auto", element: ".position4" },
+};
+
 const modelViewer = document.querySelector("#orbit-test");
 const progress = document.querySelector("#progress");
 const bar = progress.querySelector(".bar");
-const headbandHotspot = document.querySelector(".hotspot-headband");
 
-const toolbar = document
-  .querySelector(".toolbar")
-  .addEventListener("click", eventHandler);
-
-function eventHandler(e) {
-  const position = e.target.attributes[0].value;
-  const att = document.createAttribute("camera-orbit");
-
-  if (position == "position1") {
-    att.value = "90deg auto 80%";
-    modelViewer.setAttributeNode(att);
-
-    position2.style.backgroundColor = "#000";
-    position2.style.borderColor = "#0000";
-    position3.style.backgroundColor = "#000";
-    position3.style.borderColor = "#0000";
-    position4.style.backgroundColor = "#000";
-    position4.style.borderColor = "#0000";
-    position1.style.backgroundColor = "#ff0000";
-    position1.style.borderColor = "#ff0000";
-
-    // setTimeout(function () {
-    //   const pos = modelViewer.getCameraOrbit();
-    //   if (pos.theta == pos1ThetaPhi.theta && pos.phi == pos1ThetaPhi.phi) {
-    //     position1.style.backgroundColor = "#ff0000";
-    //     position1.style.borderColor = "#ff0000";
-    //   }
-    // }, 600);
-  } else if (position == "position2") {
-    att.value = "auto -180deg 90%";
-    modelViewer.setAttributeNode(att);
-
-    position1.style.backgroundColor = "#000";
-    position1.style.borderColor = "#0000";
-    position3.style.backgroundColor = "#000";
-    position3.style.borderColor = "#0000";
-    position4.style.backgroundColor = "#000";
-    position4.style.borderColor = "#0000";
-    position2.style.backgroundColor = "#ff0000";
-    position2.style.borderColor = "#ff0000";
-
-    // setTimeout(function () {
-    //   const pos = modelViewer.getCameraOrbit();
-    //   if (pos.theta == pos2ThetaPhi.theta && pos.phi == pos2ThetaPhi.phi) {
-    //     position2.style.backgroundColor = "#ff0000";
-    //     position2.style.borderColor = "#ff0000";
-    //   }
-    // }, 600);
-  } else if (position == "position3") {
-    const cameraOrbit = modelViewer.getCameraOrbit();
-    att.value = "-90deg 170deg 85%";
-    modelViewer.setAttributeNode(att);
-    position1.style.backgroundColor = "#000";
-    position1.style.borderColor = "#0000";
-    position2.style.backgroundColor = "#000";
-    position2.style.borderColor = "#0000";
-    position4.style.backgroundColor = "#000";
-    position4.style.borderColor = "#0000";
-    position3.style.backgroundColor = "#ff0000";
-    position3.style.borderColor = "#ff0000";
-
-    // setTimeout(function () {
-    //   const pos = modelViewer.getCameraOrbit();
-    //   if (pos.theta == pos3ThetaPhi.theta && pos.phi == pos3ThetaPhi.phi) {
-    //     position3.style.backgroundColor = "#ff0000";
-    //     position3.style.borderColor = "#ff0000";
-    //   }
-    // }, 600);
-  } else if (position == "position4") {
-    const cameraOrbit = modelViewer.getCameraOrbit();
-    att.value = "auto auto auto";
-    modelViewer.setAttributeNode(att);
-    position1.style.backgroundColor = "#000";
-    position1.style.borderColor = "#0000";
-    position2.style.backgroundColor = "#000";
-    position2.style.borderColor = "#0000";
-    position3.style.backgroundColor = "#000";
-    position3.style.borderColor = "#0000";
+document.querySelector(".toolbar").addEventListener("click", (e) => {
+  const positionKey = e.target.className;
+  if (positions[positionKey]) {
+    updateCameraOrbit(positions[positionKey].orbit);
+    updateButtonStyles(positionKey);
   }
-}
+});
 
 modelViewer.addEventListener("progress", (event) => {
   const { totalProgress } = event.detail;
@@ -94,13 +23,33 @@ modelViewer.addEventListener("progress", (event) => {
   bar.style.transform = `scaleX(${totalProgress})`;
 });
 
-modelViewer.addEventListener("touchend", function (e) {
-  position1.style.backgroundColor = "#000";
-  position1.style.borderColor = "#000";
-  position2.style.backgroundColor = "#000";
-  position2.style.borderColor = "#0000";
-  position3.style.backgroundColor = "#000";
-  position3.style.borderColor = "#0000";
-  position4.style.backgroundColor = "#000";
-  position4.style.borderColor = "#0000";
+modelViewer.addEventListener("touchend", () => {
+  resetButtonStyles();
 });
+
+function updateCameraOrbit(orbit) {
+  const att = document.createAttribute("camera-orbit");
+  att.value = orbit;
+  modelViewer.setAttributeNode(att);
+}
+
+function updateButtonStyles(activePosition) {
+  Object.keys(positions).forEach((key) => {
+    const element = document.querySelector(positions[key].element);
+    if (key === activePosition) {
+      element.style.backgroundColor = "#ff0000";
+      element.style.borderColor = "#ff0000";
+    } else {
+      element.style.backgroundColor = "#000";
+      element.style.borderColor = "#0000";
+    }
+  });
+}
+
+function resetButtonStyles() {
+  Object.keys(positions).forEach((key) => {
+    const element = document.querySelector(positions[key].element);
+    element.style.backgroundColor = "#000";
+    element.style.borderColor = "#0000";
+  });
+}
